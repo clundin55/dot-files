17a176f71d1f00a835def2d9d00931ee6cb3405c
Carl Lundin <108372512+clundin25@users.noreply.github.com>
2024-11-11 12:50:07 -0800

    Update neovim plugin settings. (#29)

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -2,10 +2,10 @@
 -- Set up common options
 vim.g.mapleader = " "
 vim.opt.laststatus=2
-vim.opt.tabstop=4       
-vim.opt.shiftwidth=4    
-vim.opt.softtabstop=4   
-vim.opt.expandtab=true  
+vim.opt.tabstop=4
+vim.opt.shiftwidth=4
+vim.opt.softtabstop=4
+vim.opt.expandtab=true
 vim.opt.termguicolors = true
 vim.opt.number = true
 vim.opt.spell = true
@@ -16,7 +16,9 @@ vim.opt.ruler = true
 vim.opt.undofile = true
 vim.opt.scrolloff = 8
 vim.opt.hidden = true
-vim.opt.colorcolumn="80"
+vim.opt.colorcolumn="120"
+vim.opt.swapfile = false
+
 
 -- Install packer if it's not installed
 local execute = vim.api.nvim_command
@@ -39,11 +41,13 @@ require('packer').startup(function()
     use {'wbthomason/packer.nvim', opt = true}
     use 'folke/tokyonight.nvim'
     use 'shaunsingh/nord.nvim'
+    use 'nvim-lua/plenary.nvim'
     use 'neovim/nvim-lspconfig'
     use 'tpope/vim-fugitive'
-    use 'voldikss/vim-floaterm'
     use 'tpope/vim-obsession'
+    use 'voldikss/vim-floaterm'
     use 'jremmen/vim-ripgrep'
+    use 'mrcjkb/rustaceanvim'
     use {
         'kyazdani42/nvim-tree.lua',
         requires = {
@@ -61,10 +65,16 @@ require('packer').startup(function()
     }
     use 'hrsh7th/nvim-cmp' 
     use 'hrsh7th/cmp-nvim-lsp' 
+    use 'hrsh7th/vim-vsnip' 
     use 'hrsh7th/cmp-vsnip'
+    use 'rafamadriz/friendly-snippets'
     use 'hrsh7th/cmp-path'
     use 'hrsh7th/cmp-buffer'
-    use 'simrat39/rust-tools.nvim'
+    use { 'saadparwaiz1/cmp_luasnip' }
+    use {
+        'L3MON4D3/LuaSnip',
+        config = function() require('config.snippets') end,
+    }
     use {
       'nvim-lualine/lualine.nvim',
       requires = {'kyazdani42/nvim-web-devicons', opt = true}
@@ -93,6 +103,9 @@ require'nvim-treesitter.configs'.setup {
     additional_vim_regex_highlighting = false,
   },
 }
+
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
 -- Set colorscheme through vimscript
 vim.cmd[[colorscheme tokyonight]]
@@ -126,91 +139,36 @@ vim.api.nvim_set_keymap('n', '<leader>cl', ':! cargo clippy<CR>', {noremap=true}
 vim.api.nvim_set_keymap('n', '<leader>cf', ':! cargo fmt<CR>', {noremap=true})
 vim.api.nvim_set_keymap('n', '<leader>cd', ':! cargo doc --open<CR>', {noremap=true})
 
-
 -- nice to have for neovim config
 vim.api.nvim_set_keymap('n', '<leader>ve', ':e ~/.config/nvim/init.lua<cr>', {noremap=true})
 vim.api.nvim_set_keymap('n', '<leader>vs', ':source ~/.config/nvim/init.lua<cr>', {noremap=true})
 vim.api.nvim_set_keymap('n', '<leader>vz', ':e ~/.zshrc<cr>', {noremap=true})
 
--- Set up extended rust-analyzer
-require('rust-tools').setup(opts)
-
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 local nvim_lsp = require('lspconfig')
+local configs = require('lspconfig.configs')
 
-local servers = { 'pyright', 'rust_analyzer' }
-for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup {
-    on_attach = on_attach,
-    flags = {
-      debounce_text_changes = 150,
-    }
-  }
-end
+vim.lsp.inlay_hint.enable(true)
 
 -- See `:help vim.lsp.*` for documentation on any of the below functions
 vim.api.nvim_set_keymap('n', '<leader>gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', {noremap=true})
-vim.api.nvim_set_keymap('n', '<leader>gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {noremap=true})
+vim.api.nvim_set_keymap('n', '<leader>gd', '<cmd>lua vim.lsp.buf.type_definition()<CR>', {noremap=true})
 vim.api.nvim_set_keymap('n', '<leader>K', '<cmd>lua vim.lsp.buf.hover()<CR>', {noremap=true})
 vim.api.nvim_set_keymap('n', '<leader>gq', '<cmd>lua vim.lsp.buf.implementation()<CR>', {noremap=true})
-vim.api.nvim_set_keymap('n', '<leader><C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', {noremap=true})
-vim.api.nvim_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', {noremap=true})
-vim.api.nvim_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', {noremap=true})
+vim.api.nvim_set_keymap('n', '<leader><C-k>', '<cmd>lua vim.buf.signature_help()<CR>', {noremap=true})
+vim.api.nvim_set_keymap('n', '<leader>wa', '<cmd>lua vim.buf.add_workspace_folder()<CR>', {noremap=true})
+vim.api.nvim_set_keymap('n', '<leader>wr', '<cmd>lua vim.buf.remove_workspace_folder()<CR>', {noremap=true})
 vim.api.nvim_set_keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', {noremap=true})
-vim.api.nvim_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', {noremap=true})
+vim.api.nvim_set_keymap('n', '<leader>D', '<cmd>lua vim.buf.type_definition()<CR>', {noremap=true})
 vim.api.nvim_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', {noremap=true})
 vim.api.nvim_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', {noremap=true})
 vim.api.nvim_set_keymap('n', '<leader>gr', '<cmd>lua vim.lsp.buf.references()<CR>', {noremap=true})
 vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', {noremap=true})
-vim.api.nvim_set_keymap('n', '<leader>[d', '<cmd>lua vim.diagnostic.get_prev()<CR>', {noremap=true})
-vim.api.nvim_set_keymap('n', '<leader>]d', '<cmd>lua vim.diagnostic.get_next()<CR>', {noremap=true})
-vim.api.nvim_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.get_loclist()<CR>', {noremap=true})
+vim.api.nvim_set_keymap('n', '<leader>[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', {noremap=true})
+vim.api.nvim_set_keymap('n', '<leader>]d', '<cmd>lua vim.diagnostic.goto_next()<CR>', {noremap=true})
+vim.api.nvim_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.set_loclist()<CR>', {noremap=true})
 vim.api.nvim_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', {noremap=true})
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-  -- Enable completion triggered by <c-x><c-o>
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  local opts = { 
-      tools = {
-            autoSetHints = true,
-            hover_with_actions = true,
-            runnables = {
-                use_telescope = true
-            },
-            inlay_hints = {
-                show_parameter_hints = false,
-                parameter_hints_prefix = "",
-                other_hints_prefix = "",
-            },
-        },
-        -- all the opts to send to nvim-lspconfig
-        -- these override the defaults set by rust-tools.nvim
-        -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
-        server = {
-            -- on_attach is a callback called when the language server attachs to the buffer
-            -- on_attach = on_attach,
-            settings = {
-                -- to enable rust-analyzer settings visit:
-                -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
-                ["rust-analyzer"] = {
-                    -- enable clippy on save
-                    checkOnSave = {
-                        command = "clippy"
-                    },
-                }
-            }
-        },
-    }
-end
-
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
@@ -299,3 +257,9 @@ require'lualine'.setup {
 require'nvim-web-devicons'.setup {
  default = true;
 }
+vim.api.nvim_set_keymap("i", "<c-j>", "<cmd>lua require'luasnip'.jump(1)<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("s", "<c-j>", "<cmd>lua require'luasnip'.jump(1)<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<c-k>", "<cmd>lua require'luasnip'.jump(-1)<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("s", "<c-k>", "<cmd>lua require'luasnip'.jump(-1)<CR>", { noremap = true, silent = true })
+
+require("luasnip.loaders.from_vscode").lazy_load()
